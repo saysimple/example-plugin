7af49d388eb02eb2701ab950f90c5a586214a8c6
Thom Werring <thom@netexpo.nl>
2022-03-22 08:19:55 +0100

    Added bootstrap vue as dev dependencies

--- src/settings.vue
+++ src/settings.vue
@@ -1,81 +1,81 @@
 <template>
-  <section>
-    <form id="lef" method="post" @submit="submitForm">
-      <input class="button save" type="submit" :value='$t("Save")'/>
-    </form>
-  </section>
+    <section>
+        <form id="lef" method="post" @submit="submitForm">
+            <pre>
+                {{ JSON.stringify(plugin, null, "\t") }}
+            </pre>
+
+            <input class="button save" type="submit" :value="$t('Save')" />
+        </form>
+    </section>
 </template>
 
 <style scoped>
-
 #apiUrl {
-  width: 255px;
+    width: 255px;
 }
 
 .menu-item textarea {
-  width: 300px;
+    width: 300px;
 }
-
 </style>
 
 <script>
-
 export default {
-  components: {
-  },
-  props: {
-    plugin: {
-      type: Object,
-      default: {},
-    },
-  },
-  data() {
-    return {
-      carModels: null,
-      active: null,
-      apiUrl: null,
-      apiUser: null,
-      apiKey: null,
-      apiUrls: [
-        {
-          label: this.$t("Live"),
-          url: "https://api.mijnlef.nl/leads/new/",
+    components: {},
+    props: {
+        plugin: {
+            type: Object,
+            default: {},
         },
-        {
-          label: this.$t("Acceptance"),
-          url: "https://api-acc.mijnlef.nl/leads/new/",
-        },
-      ],
-    };
-  },
-  created() {
-    // Load plugin settings into model
-    this.carModels = this.plugin.getSetting("carModels").join("\n");
-    this.active    = this.plugin.getSetting("active");
-    this.apiUrl    = this.plugin.getSetting("api.url");
-    this.apiUser   = this.plugin.getSetting("api.username");
-    this.apiKey    = this.plugin.getSetting("api.password");
-  },
-  methods: {
-    toggleActive() {
-      this.active ^= 1; // Toggle between 0 and 1
-      return this.active;
     },
-    submitForm(event) {
-      event.preventDefault();
+    data() {
+        return {
+            carModels: null,
+            active: null,
+            apiUrl: null,
+            apiUser: null,
+            apiKey: null,
+            apiUrls: [
+                {
+                    label: this.$t("Live"),
+                    url: "https://api.mijnlef.nl/leads/new/",
+                },
+                {
+                    label: this.$t("Acceptance"),
+                    url: "https://api-acc.mijnlef.nl/leads/new/",
+                },
+            ],
+        };
+    },
+    created() {
+        // Load plugin settings into model
+        // this.carModels = this.plugin.getSetting("carModels").join("\n");
+        // this.active = this.plugin.getSetting("active");
+        // this.apiUrl = this.plugin.getSetting("api.url");
+        // this.apiUser = this.plugin.getSetting("api.username");
+        // this.apiKey = this.plugin.getSetting("api.password");
+    },
+    methods: {
+        toggleActive() {
+            this.active ^= 1; // Toggle between 0 and 1
+            return this.active;
+        },
+        submitForm(event) {
+            event.preventDefault();
 
-      $nuxt.$emit("savePluginSettings", {
-        name: this.plugin.name,
-        active: Number(this.active),
-        carModels: this.carModels.split("\n").filter((model) => model),
-        api: {
-          url: this.apiUrl,
-          username: this.apiUser,
-          password: this.apiKey,
-        }
-      });
-    }
-  },
+            $nuxt.$emit("savePluginSettings", {
+                name: this.plugin.name,
+                active: Number(this.active),
+                carModels: this.carModels.split("\n").filter((model) => model),
+                api: {
+                    url: this.apiUrl,
+                    username: this.apiUser,
+                    password: this.apiKey,
+                },
+            });
+        },
+    },
 };
 </script>
 
